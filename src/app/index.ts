a4bf4cfb379ff1ad0accab967408b9eafc875028
Collin <collin.huynh.gos@gmail.com>
2024-12-17 17:34:52 +0700

    feat: Enable CORS policy

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,13 +1,22 @@
-import { INestApplication } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { ConfigService } from "@nestjs/config";
-import { configAppDocument } from "./app.document";
-import { configAppInterceptor } from "./app.interceptor";
+import { INestApplication } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { configAppDocument } from './app.document';
+import { configAppInterceptor } from './app.interceptor';
 
 export const initApplication = async (): Promise<INestApplication> => {
   const app = await NestFactory.create(AppModule, { abortOnError: true });
   const configService = app.get(ConfigService);
+
+  const origins = configService.get<string>('CORS_ORIGINS', '');
+
+  app.enableCors({
+    origin: origins.split(','),
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
   await configAppInterceptor(app);
   await configAppDocument(app, configService);
